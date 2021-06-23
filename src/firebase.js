11c4631b067056fe580227d86370924e0eee7586
Daniel Andres Gallo Garcia <dany338@gmail.com>
2021-06-22 21:01:52 -0500

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDfVkYNuW_5MQqqd56BJMfWKWWh4nkHKjE",
-  authDomain: "movieshowtimefinder-2346b.firebaseapp.com",
-  databaseURL: "https://movieshowtimefinder-2346b.firebaseio.com",
-  projectId: "movieshowtimefinder-2346b",
-  storageBucket: "movieshowtimefinder-2346b.appspot.com",
-  messagingSenderId: "236955615103",
-  appId: "1:236955615103:web:24f5a578e7c5970464644a"
+  apiKey: "AIzaSyDEVexb_DfBKr6PJPWHPRwK2P0Jc6K1svc",
+  authDomain: "movielist-1f0ef.firebaseapp.com",
+  projectId: "movielist-1f0ef",
+  storageBucket: "movielist-1f0ef.appspot.com",
+  messagingSenderId: "120835898563",
+  appId: "1:120835898563:web:99f1a8877d7f36ec62b676"
 });
 
 const db = firebaseApp.firestore();
